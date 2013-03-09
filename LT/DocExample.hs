83dd919415d8219f048f38068a576f2157c66f50
MoritaYasuaki <zousandazou@gmail.com>
2013-03-09 14:51:51 +0900

    modify

--- LT/DocExample.hs
+++ LT/DocExample.hs
@@ -1,35 +1,25 @@
--- | 
--- This module is an example of how to use doctest and haddock.
--- Unfortunately, haddock don't supoprt UTF-8 and `prop>` notation.
+{- |
+Module :   DocExample
+Description :   doctest-haddock-example
+Copyright :   (c) Morita Yasuaki
+License :   BSD liscense
+-}
 
 module DocExample where
 
 -- |
--- Test cases of `double`
---
--- >>> double 2
--- 4
+-- doubleのテストケースだよ
 -- >>> double 4
 -- 8
--- 
--- `double`'s properties :
---
--- @
---   prop> double x `div` 2 == x
---   prop> even x ==> double (x `div` 2) == x
---   prop> odd x  ==> double (x `div` 2) == x - 1
--- @
 
 double :: Int -> Int
-double a = 2 * a
+double a = 2*a
 
-data Tree a = Empty | Node a (Tree a) (Tree a) deriving Show
+-- ^
+-- doubleの他の利用例
+-- >>> double 12
+-- 24
+--
+-- double関数の性質
+-- prop> double x `div` 2 == x
 
--- |
--- >>> children Empty
--- []
--- >>> children (Node 1 (Node 2 Empty Empty) (Node 3 Empty Empty))
--- [Node 2 Empty Empty,Node 3 Empty Empty]
-
-children Empty = []
-children (Node a left right) = [left, right]
