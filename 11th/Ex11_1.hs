d832eab9e98767a51d97de072deb95c9da682b9c
MoritaYasuaki <zousandazou@gmail.com>
2013-04-19 23:18:39 +0900

    abstraction parameter

--- 11th/Ex11_1.hs
+++ 11th/Ex11_1.hs
@@ -3,6 +3,15 @@ module Main where
 import System.Random
 import Text.Printf
 
+lower :: Char
+lower = '0'
+
+upper :: Char
+upper = '9'
+
+len :: Int
+len = 4
+
 main :: IO ()
 main = do
     secret <- mkSecret
@@ -12,31 +21,36 @@ main = do
 mkSecret :: IO String
 mkSecret = do
     gen <- newStdGen
-    return . take 4 $ randomRs ('0', '9') gen
+    return . take len $ randomRs (lower, upper) gen
 
 game :: String -> IO ()
 game secret = do
     putStr "> "
     input <- getLine
     case check secret input of
-        InputError -> putStrLn "Please input 4 digits." >> game secret
-        Incorrect (a,b) -> putStrLn (printf "%d %d" a b) >> game secret
-        Correct -> putStrLn "Correct!"
+        InputError -> 
+          putStrLn (printf "Please input %d digits." len) 
+            >> game secret
+        Incorrect a b -> 
+          putStrLn (printf "%d %d" a b) 
+            >> game secret
+        Correct -> 
+          putStrLn "Correct!"
 
 data Result = InputError
-            | Incorrect (Int,Int)
+            | Incorrect Int Int
             | Correct
             deriving Show
 
 -- |
 -- >>> check "1234" "1111"
--- Incorrect (1,1)
+-- Incorrect 1 1
 -- >>> check "1234" "2222"
--- Incorrect (1,1)
+-- Incorrect 1 1
 -- >>> check "1234" "3210"
--- Incorrect (1,3)
+-- Incorrect 1 3
 -- >>> check "1234" "0123"
--- Incorrect (0,3)
+-- Incorrect 0 3
 -- >>> check "1234" "1234"
 -- Correct
 -- >>> check "1234" "test"
@@ -47,10 +61,10 @@ data Result = InputError
 
 check :: String -> String -> Result
 check secret input
-    | length input /= 4 || any (`notElem` ['0'..'9']) input
-      = InputError
-    | secret == input = Correct
-    | otherwise = Incorrect (numMatched, numExists)
+    | length input /= len || any (`notElem` [lower..upper]) input
+                       = InputError
+    | secret == input  = Correct
+    | otherwise        = Incorrect numMatched numExists
     where numMatched = numTrue $ zipWith (==) secret input
           numExists = numTrue $ map (`elem` input) secret
           numTrue = length . filter (== True)
